18d1cf87128abecbc6e34db921b52dde923520f7
Adrian Horning <m.adrian.horning@gmail.com>
2018-06-02 18:21:52 -0700

    Logging in to Linkedin

--- google_test.js
+++ google_test.js
@@ -1,14 +1,20 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
+const creds = require('./config');
 
 (async function example() {
   let driver = await new Builder().forBrowser('chrome').build();
   try {
-    await driver.get('http://www.google.com/ncr');
-    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-    el = await driver.findElement(By.tagName('div'));
-    console.log(el);
-    await el.getText(text => console.log(text));
+    await driver.get('https://www.linkedin.com/');
+    let email = await driver.findElement(By.id('login-email'));
+    let password = await driver.findElement(By.id('login-password'));
+    email.sendKeys('m.adrian.horning@gmail.com')
+    password.sendKeys('liahona7')
+    try {
+      await driver.findElement(By.id('login-submit').click());
+    } catch(err) {
+      console.log(err);
+    }
   } finally {
-    await driver.quit();
+    // await driver.quit();
   }
 })();
