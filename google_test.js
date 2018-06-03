97969bf6aa702efd0da332842256578bdb40fd4d
Adrian Horning <m.adrian.horning@gmail.com>
2018-06-02 19:08:20 -0700

    Added an await

--- google_test.js
+++ google_test.js
@@ -1,6 +1,8 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
 const creds = require('./config.js');
 
+// sometimes it doesn't work. Gotta somehow try an action again if it fails
+
 (async function example() {
   let driver = await new Builder().forBrowser('chrome').build();
   try {
@@ -12,8 +14,9 @@ const creds = require('./config.js');
     try {
       await driver.findElement(By.id('login-submit')).click();
       let searchBox = await driver.findElement(By.tagName('input'));
-      searchBox.sendKeys('asana')
-      let searchButton = await driver.findElement(By.className('search-typeahead-v2__button typeahead-icon')).click();
+      await searchBox.sendKeys('asana engineering')
+      await driver.findElement(By.className('search-typeahead-v2__button typeahead-icon')).click()
+      
     } catch(err) {
       console.log(err);
     }
